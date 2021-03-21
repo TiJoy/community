196ec439a46e1cd12cc9d1ea9a59e91f8fb97d8b
litengjiao <tjli.xdu@gmail.com>
2021-03-21 16:30:19 +0800

    第二次提交

--- src/test/java/com/nowcoder/community/LoggerTests.java
+++ src/test/java/com/nowcoder/community/LoggerTests.java
@@ -6,6 +6,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @SpringBootTest
 @ContextConfiguration(classes = CommunityApplication.class)
 public class LoggerTests {
@@ -20,5 +25,22 @@ public class LoggerTests {
         logger.info("info log");
         logger.warn("warn log");
         logger.error("error log");
+    }
+
+    @Test
+    public void test() {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(1, 1);
+        map.put(2, 2);
+        map.put(3, 3);
+        map.put(4, 4);
+        for(Iterator<Map.Entry<Integer, Integer>> it = map.entrySet().iterator(); it.hasNext();) {
+            System.out.println(it.next());
+            it.remove();
+        }
+
+        for(Iterator<Map.Entry<Integer, Integer>> it = map.entrySet().iterator(); it.hasNext();) {
+            System.out.println(it.next());
+        }
     }
 }
